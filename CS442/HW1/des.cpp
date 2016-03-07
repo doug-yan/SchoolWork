2630636c24648e2b3e0ad6299ba33ffe3a8b6e80
PairForce.io <session@pairforce.io>
2016-03-07 05:30:32 +0000

    There has to be a better way to do this.`

---
+++ CS442/HW1/des.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <bitset>
+#include <string>
+
+using namespace std;
+
+void initPermutation(int input[], int output[]);
+
+int main(){
+  int plaintextBinary[64];
+  int keyBinary[64];
+  
+  return 0;
+}
+
+void initPermutation(int input[], int output[]) {
+  output[0] = input[57];
+  output[1] = input[49];
+  output[2] = input[41];
+  output[3] = input[33];
+  output[4] = input[25];
+  output[5] = input[17];
+  output[6] = input[9];
+  output[7] = input[1];
+  output[8] = input[59];
+  output[9] = input[51];
+  output[10] = input[43];
+  output[11] = input[35];
+  output[12] = input[];
+  output[13] = input[];
+  output[14] = input[];
+  output[15] = input[];
+  output[16] = input[];
+  output[17] = input[];
+  output[18] = input[];
+  output[19] = input[];
+  output[20] = input[];
+  output[21] = input[];
+  output[22] = input[];
+  output[23] = input[];
+  output[24] = input[];
+  output[25] = input[];
+  output[26] = input[];
+  output[27] = input[];
+  output[28] = input[];
+  output[29] = input[];
+  output[30] = input[];
+  output[31] = input[];
+  output[32] = input[];
+  output[33] = input[];
+  output[34] = input[];
+  output[35] = input[];
+  output[36] = input[];
+  output[37] = input[];
+  output[38] = input[];
+  output[39] = input[];
+  output[40] = input[];
+  output[41] = input[];
+  output[42] = input[];
+  output[43] = input[];
+  output[44] = input[];
+  output[45] = input[];
+  output[46] = input[];
+  output[47] = input[];
+  output[48] = input[];
+  output[49] = input[];
+  output[50] = input[];
+  output[51] = input[];
+  output[52] = input[];
+  output[53] = input[];
+  output[54] = input[];
+  output[55] = input[];
+  output[56] = input[];
+  output[57] = input[];
+  output[58] = input[];
+  output[59] = input[];
+  output[60] = input[];
+  output[61] = input[];
+  output[62] = input[];
+  output[63] = input[];
+}
